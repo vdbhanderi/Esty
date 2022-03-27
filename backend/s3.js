d8169382e7238ffdd266416348d17b0107b5df85
vdbhanderi <99316328+vdbhanderi@users.noreply.github.com>
2022-03-26 21:27:10 -0700

    Update S3 bucket file

--- backend/s3.js
+++ backend/s3.js
@@ -5,9 +5,6 @@ const bucketName=process.env.AWS_BUCKET_NAME
 const region=process.env.AWS_BUCKET_REGION
 const accessKey=process.env.AWS_ACCESS_KEY
 const secreatKey=process.env.AWS_SECRET_KEY
-AWS_BUCKET_REGION="us-east-1"
-AWS_ACCESS_KEY="AKIA4SYTREPCOFKXJ2MY"
-AWS_SECRET_KEY="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"
 
 const s3=new S3({
     region,
